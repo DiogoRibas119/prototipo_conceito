1a2040b44864129a25fdf42000b75dcee3411edd
root <root@faiserver>
2022-03-23 10:55:40 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { create } from 'ipfs-http-client'
+//const ipfs = create(new URL('http://172.31.3.80:5001'))
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: '172.31.3.80', port: 5001, protocol: 'http' }) 
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      buffer: null
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    console.log("submit")
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log("resultado", result)
+      if(error) {
+        console.error(error)
+        return
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />          
+          <p></p><p></p><p></p><p></p>
+          <h2>Add File</h2>
+          <form onSubmit={this.onSubmit} >
+          <input type='file' onChange={this.captureFile} />
+          <input type='submit' />
+        </form>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
